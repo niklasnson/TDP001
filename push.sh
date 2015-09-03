6af9a1c2e1c2256d0d4b7e6c70cbe559b046313c
niklas nson <niklasnson@me.com>
2015-09-03 07:54:48 +0200

    adds .sh file

---
+++ push.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+scp ~/Projects/etc/http.tdp001/index.html nikni292@remote-und.ida.liu.se:www-pub/TDP001
+scp ~/Projects/etc/http.tdp001/main.css nikni292@remote-und.ida.liu.se:www-pub/TDP001
